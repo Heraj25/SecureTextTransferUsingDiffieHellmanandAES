68e6ca7a8576921322021bb3592ce3e6f144de40
Heraj25 <herajgantyada@gmail.com>
2024-05-01 22:57:38 +0530

    new changes

--- src/ecryptiondecryption/EDcrypt.java
+++ src/ecryptiondecryption/EDcrypt.java
@@ -40,7 +40,7 @@ public class EDcrypt extends javax.swing.JFrame {
     }
 
  
-    @SuppressWarnings("unchecked")
+    
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
